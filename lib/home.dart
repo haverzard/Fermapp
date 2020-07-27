6a03f8ea0797a16eae75abf597f7c619063bbd69
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-07-27 17:49:35 +0700

    Use listview builder for app listing

--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<Map> appsList = [
+    {
+      "appName": "APP NAME",
+      "logoPath": "assets/Lulu.PNG",
+      "remainingTime": "30s",
+    },
+    {
+      "appName": "APP NAME",
+      "logoPath": "assets/Lulu.PNG",
+      "remainingTime": "30s",
+    },
+    {
+      "appName": "APP NAME",
+      "logoPath": "assets/Lulu.PNG",
+      "remainingTime": "30s",
+    },
+    {
+      "appName": "APP NAME",
+      "logoPath": "assets/Lulu.PNG",
+      "remainingTime": "30s",
+    },
+    {
+      "appName": "APP NAME",
+      "logoPath": "assets/Lulu.PNG",
+      "remainingTime": "30s",
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
@@ -46,7 +74,7 @@ class _HomeState extends State<Home> {
                           ),
                           Container(
                             decoration: BoxDecoration(
-                                color: Colors.blue,
+                                color: Color.fromRGBO(255, 111, 111, 1),
                                 borderRadius: BorderRadius.all(Radius.circular(20))
                             ),
                             width: MediaQuery.of(context).size.width * 0.9,
@@ -73,7 +101,7 @@ class _HomeState extends State<Home> {
                           ),
                           Container(
                             decoration: BoxDecoration(
-                                color: Colors.blue,
+                                color: Color.fromRGBO(255, 111, 111, 1),
                                 borderRadius: BorderRadius.all(Radius.circular(20))
                             ),
                             width: MediaQuery.of(context).size.width * 0.9,
@@ -87,35 +115,67 @@ class _HomeState extends State<Home> {
               ),
             ),
             Container(
-              height: 300,
-              child: ListView(
+              height: 350,
+              child: ListView.builder(
                 scrollDirection: Axis.vertical,
-                children: <Widget>[
-                  Container(
-                    height: 75,
-                    child: Row(
-                      children: <Widget>[
-                        SizedBox(width: 100),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              "APP NAME",
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
+                itemCount: appsList.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                      padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      height: 75,
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
+                            child: Container(
+                                width: 55,
+                                height: 55,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: AssetImage("${appsList[index]["logoPath"]}")
+                                    )
+                                )
                             ),
-                            Text("remaining time: 30s")
-                          ],
-                        )
-                      ],
-                    )
-                  ),
-                  Container(
-                      height: 50,
-                      child: Text("Test")
-                  )
-                ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "${appsList[index]["appName"]}",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Text("remaining time: ${appsList[index]["remainingTime"]}")
+                            ],
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                            width: 90,
+                            child: RaisedButton(
+                                onPressed: () {},
+                                color: Color.fromRGBO(172, 238, 106, 1),
+                                textColor: Colors.black,
+                                child: Text("Edit")
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                            width: 80,
+                            child: RaisedButton(
+                                onPressed: () {},
+                                color: Color.fromRGBO(87, 160, 246, 1),
+                                textColor: Colors.black,
+                                child: Text("Info")
+                            ),
+                          )
+                        ],
+                      )
+                  );
+                }
               ),
             ),
           ]
